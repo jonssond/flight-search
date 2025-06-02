196c16418165204223e5104a4508f5b5f61059ff
Diogo Jonsson <diogo.jonsson@hotmail.com>
2025-06-02 20:55:22 -0300

    feat: add FlightController to manage flight-related requests

---
+++ src/controller/flight.controller.ts
@@ -0,0 +1,19 @@
+import { Request, Response } from "express";
+import { FlightService } from "../service/flight.service";
+
+export class FlightController {
+    private flightService: FlightService;
+
+    constructor() {
+        this.flightService = new FlightService();
+    }
+
+    async getAll(req: Request, res: Response) {
+        try {
+            const flights = await this.flightService.getAll();
+            res.status(200).json(flights);
+        } catch(error) {
+            res.status(500).json({ message: 'Error fetching flights', error });
+        }
+    }
+} 
