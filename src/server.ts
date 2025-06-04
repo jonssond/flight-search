4797bf3e5068cdaa7ef32fa91f884e6cc7008721
Diogo Jonsson <diogo.jonsson@hotmail.com>
2025-06-04 17:00:46 -0300

    feat: add cors dependency and its type definitions

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,25 @@
 import 'reflect-metadata';
 import express, { Application, Request, Response } from 'express';
 import { AppDataSource } from './config/database/data-source';
+<<<<<<< Updated upstream
+=======
+import flightsRouter from './route/flight.route';
+import cors from 'cors';
+>>>>>>> Stashed changes
 
 const app: Application = express();
 const port: number = parseInt(process.env.PORT || '3000');
 
 app.use(express.json());
+<<<<<<< Updated upstream
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello, World!');
 });
+=======
+app.use(cors());
+app.use('/', flightsRouter);
+>>>>>>> Stashed changes
 
 AppDataSource.initialize()
   .then(() => {
