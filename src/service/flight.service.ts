ac623105dcf46d2f88bf81748615c7acaa93d829
jonssond <diogo.jonsson@hotmail.com>
2025-06-05 11:04:46 -0300

    feat: add pagination in service

--- src/service/flight.service.ts
+++ src/service/flight.service.ts
@@ -1,10 +1,31 @@
-import { Flight } from "../entity/flight.entity"; 
-import { AppDataSource } from "../config/database/data-source";
+// src/service/flight.service.ts
+import { Flight } from "../entity/flight.entity";
+import { FlightRepository } from "../repository/FlightRepository";
+
+export interface PaginatedFlightServiceResponse {
+    flights: Flight[];
+    currentPage: number;
+    totalPages: number;
+    totalItems: number;
+    itemsPerPage: number;
+}
 
 export class FlightService {
-    private flightRepository = AppDataSource.getRepository(Flight);
+    private flightRepository: FlightRepository;
 
-    async getAll(): Promise<Flight[]> {
-        return this.flightRepository.find();
+    constructor() {
+        this.flightRepository = new FlightRepository();
+    }
+
+    async getAll(page: number, limit: number): Promise<PaginatedFlightServiceResponse> {
+        const { data, total } = await this.flightRepository.getAll(page, limit);
+
+        return {
+            flights: data,
+            currentPage: page,
+            totalPages: Math.ceil(total / limit),
+            totalItems: total,
+            itemsPerPage: limit,
+        };
     }
 }
