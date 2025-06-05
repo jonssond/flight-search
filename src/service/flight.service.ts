bd5277eafaa7d46c23b3b417916cb95a5c03067a
Diogo Jonsson <diogo.jonsson@hotmail.com>
2025-06-05 14:38:49 -0300

    feat: enhance getAll method in FlightService to support filtering

--- src/service/flight.service.ts
+++ src/service/flight.service.ts
@@ -17,15 +17,46 @@ export class FlightService {
         this.flightRepository = new FlightRepository();
     }
 
-    async getAll(page: number, limit: number): Promise<PaginatedFlightServiceResponse> {
-        const { data, total } = await this.flightRepository.getAll(page, limit);
-
-        return {
-            flights: data,
-            currentPage: page,
-            totalPages: Math.ceil(total / limit),
-            totalItems: total,
-            itemsPerPage: limit,
-        };
+    async getAll(page: number, limit: number, filters: any) {
+    const skip = (page - 1) * limit;
+    
+    const whereCondition: any = {};
+    
+    if (filters.origin) {
+      whereCondition.origin = { contains: filters.origin, mode: 'insensitive' };
     }
+    
+    if (filters.destination) {
+      whereCondition.destination = { contains: filters.destination, mode: 'insensitive' };
+    }
+    
+    if (filters.departureDate) {
+      const startOfDay = new Date(filters.departureDate);
+      startOfDay.setHours(0, 0, 0, 0);
+      const endOfDay = new Date(filters.departureDate);
+      endOfDay.setHours(23, 59, 59, 999);
+      
+      whereCondition.departure = {
+        gte: startOfDay,
+        lte: endOfDay,
+      };
+    }
+    
+    if (filters.arrivalDate) {
+      const startOfDay = new Date(filters.arrivalDate);
+      startOfDay.setHours(0, 0, 0, 0);
+      const endOfDay = new Date(filters.arrivalDate);
+      endOfDay.setHours(23, 59, 59, 999);
+      
+      whereCondition.arrival = {
+        gte: startOfDay,
+        lte: endOfDay,
+      };
+    }
+
+    console.log("CONDIÇÃO WHERE: (flight.service.ts)", whereCondition);
+
+    return await this.flightRepository.getAllWithFilters(whereCondition, skip, limit);
+
+  }
 }
